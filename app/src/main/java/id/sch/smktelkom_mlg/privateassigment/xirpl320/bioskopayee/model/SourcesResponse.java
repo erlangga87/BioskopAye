f5afde5b28f6190e6f21439acf0e6131a91a002c
deva <deva.erlangga87@gmail.com>
2017-06-13 15:02:41 +0700

    done

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xirpl320/bioskopayee/model/SourcesResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassigment.xirpl320.bioskopayee.model;
+
+import java.util.List;
+
+/**
+ * Created by Mbrew on 5/14/2017.
+ */
+
+public class SourcesResponse {
+    public String page;
+    public List<Source> results;
+
+}
